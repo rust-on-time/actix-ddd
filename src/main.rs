2d79de961fddbcc067e0a5c7a7184f7a56354566
Uriah <codeitlikemiley@gmail.com>
2024-07-20 00:38:42 +0800

    delete main

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use arch::infrastructure::web::run;
-use env_logger::Env;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok();
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    run().await
-}
