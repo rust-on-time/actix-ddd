c80ca53017feffa342886958de27fe44a6b08d27
Uriah <codeitlikemiley@gmail.com>
2024-07-19 23:50:17 +0800

    add compile time check

--- src/application/dto/new_user_dto.rs
+++ src/application/dto/new_user_dto.rs
@@ -5,6 +5,7 @@ use crate::schema::users;
 
 #[derive(Debug, Clone, Deserialize, Insertable)]
 #[diesel(table_name = users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct NewUser {
     pub name: String,
     pub email: String,
--- src/domain/entities/user.rs
+++ src/domain/entities/user.rs
@@ -2,6 +2,8 @@ use diesel::deserialize::Queryable;
 use serde::Serialize;
 
 #[derive(Debug, Serialize, Queryable, Clone)]
+#[diesel(table_name = crate::schema::posts)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     pub id: i32,
     pub name: String,
