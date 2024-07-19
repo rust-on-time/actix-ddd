5617bb3ca0425b20d86f5d000e33573bbc1d1351
Uriah <codeitlikemiley@gmail.com>
2024-07-19 22:16:49 +0800

    add NewUser

--- src/domain/repositories/user_repository.rs
+++ src/domain/repositories/user_repository.rs
@@ -1,9 +1,9 @@
 use async_trait::async_trait;
 
-use crate::domain::entities::User;
+use crate::{domain::entities::User, presentation::handlers::NewUser};
 
 #[async_trait]
 pub trait UserRepository {
     async fn find_by_email(&self, email: &str) -> Option<User>;
-    async fn save(&self, user: &User) -> Result<(), diesel::result::Error>;
+    async fn save(&self, user: &NewUser) -> Result<(), diesel::result::Error>;
 }
