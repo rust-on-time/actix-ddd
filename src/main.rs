bc1ccc2c4f493f71d95b049d6a1c812d6e5d8096
Uriah <codeitlikemiley@gmail.com>
2024-07-19 22:46:37 +0800

    run main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use arch::infrastructure::web::run;
+use env_logger::Env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    run().await
 }
