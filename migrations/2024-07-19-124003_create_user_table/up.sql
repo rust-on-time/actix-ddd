22d71ae75b30154c92e99bc0207cf56d9b7c98d8
Uriah <codeitlikemiley@gmail.com>
2024-07-19 23:36:12 +0800

    make username unique

--- migrations/2024-07-19-124003_create_user_table/up.sql
+++ migrations/2024-07-19-124003_create_user_table/up.sql
@@ -3,7 +3,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL
 );
