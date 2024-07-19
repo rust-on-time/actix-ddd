fe60756388426712729835492ef1920b04415894
Uriah <codeitlikemiley@gmail.com>
2024-07-19 22:54:18 +0800

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     run().await
 }
