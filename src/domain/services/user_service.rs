77316dfa8fce509a30a771dc9db8ef9659d5f1f4
Uriah <codeitlikemiley@gmail.com>
2024-07-19 21:10:18 +0800

    added user_service

---
+++ src/domain/services/user_service.rs
@@ -0,0 +1,24 @@
+use crate::domain::{entities::User, repositories::UserRepository};
+
+pub struct UserService<T>
+where
+    T: UserRepository,
+{
+    user_repo: T,
+}
+
+impl<T: UserRepository> UserService<T> {
+    pub fn new(new_repo: T) -> Self {
+        UserService {
+            user_repo: new_repo,
+        }
+    }
+
+    pub async fn register(&self, user: &User) -> Result<(), diesel::result::Error> {
+        self.user_repo.save(user).await
+    }
+
+    pub async fn find_by_email(&self, email: &str) -> Option<User> {
+        self.user_repo.find_by_email(email).await
+    }
+}
